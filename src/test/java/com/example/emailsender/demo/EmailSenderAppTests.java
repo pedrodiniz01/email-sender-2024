348438d959210f3157b005dca5d16e2dde7d57d1
pedrodiniz01 <1170053@isep.ipp.pt>
2024-09-14 22:53:15 +0100

    renaem app

--- src/test/java/com/example/emailsender/demo/EmailSenderAppTests.java
+++ src/test/java/com/example/emailsender/demo/EmailSenderAppTests.java
@@ -1,9 +1,10 @@
 package com.example.emailsender.demo;
 
+import com.example.emailsender.app.EmailSenderApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EmailSenderApp.class)
 class EmailSenderAppTests {
 
 	@Test
