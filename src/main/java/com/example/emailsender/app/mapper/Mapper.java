a8cff46f7ec50c9c67abbbd554bb90ea446270d9
pedrodiniz01 <1170053@isep.ipp.pt>
2024-10-06 20:51:02 +0100

    add more logic mapper

--- src/main/java/com/example/emailsender/app/mapper/Mapper.java
+++ src/main/java/com/example/emailsender/app/mapper/Mapper.java
@@ -1,10 +1,14 @@
 package com.example.emailsender.app.mapper;
 
+import com.example.emailsender.app.dtos.CreateAdditionalMessageOutputDto;
 import com.example.emailsender.app.dtos.CreateMessageInputDto;
 import com.example.emailsender.app.dtos.CreateMessageOutputDto;
 import com.example.emailsender.app.dtos.ScheduleDto;
+import com.example.emailsender.app.repository.tables.AdditionalMessageJPA;
 import com.example.emailsender.app.repository.tables.MessageJpa;
 import com.example.emailsender.app.repository.tables.ScheduleJpa;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -15,8 +19,22 @@ public interface Mapper {
     MessageJpa toMessageJpa(CreateMessageInputDto dto);
     ScheduleJpa toScheduleJpa(ScheduleDto dto);
     List<CreateMessageOutputDto> toMessageOutputDtoList(List<MessageJpa> jpaList);
+    @Mapping(target = "additionalMessages", qualifiedByName = "sortAdditionalMessagesByIdDesc")
     CreateMessageOutputDto toMessageOutputDto(MessageJpa messageJpa);
     List<ScheduleDto> toScheduleDtoList(List<ScheduleJpa> jpaList);
     ScheduleDto toScheduleDto(ScheduleJpa jpa);
     List<CreateMessageInputDto> toCreateMessageInputDtoList (List<CreateMessageOutputDto> createMessageOutputDtos);
+    CreateAdditionalMessageOutputDto additionalMessageJPAToCreateAdditionalMessageOutputDto(AdditionalMessageJPA additionalMessageJPA);
+
+    @Named("sortAdditionalMessagesByIdDesc")
+    default List<CreateAdditionalMessageOutputDto> sortAdditionalMessagesByIdDesc(List<AdditionalMessageJPA> additionalMessages) {
+        if (additionalMessages == null) {
+            return null;
+        }
+
+        return additionalMessages.stream()
+                .sorted((a1, a2) -> Long.compare(a2.getId(), a1.getId()))
+                .map(this::additionalMessageJPAToCreateAdditionalMessageOutputDto)
+                .toList();
+    }
 }
